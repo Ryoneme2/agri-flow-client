9246fc864cf824b16603960f2e7357ea423c513a
Ryoneme2 <worknarair6@gmail.com>
2022-11-11 04:43:53 +0700

    fix google login

--- pages/testGoogleLogin.jsx
+++ pages/testGoogleLogin.jsx
@@ -1,69 +1,71 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import Image from 'next/image';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import axios from 'axios';
 
 const TestGoogleLogin = () => {
   // Provide a custom `fetch` implementation as an option
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  const [profile, setProfile] = useState([]);
+  const [person, setPerson] = useState({});
+
   useEffect(() => {
-    const x = async () => {
-      const gapi = await import('gapi-script').then((pack) => pack.gapi);
-      const initClient = () => {
-        gapi.client.init({
-          clientId: clientId,
-          scope: '',
-        });
-      };
-      gapi.load('client:auth2', initClient);
+    const getData = async () => {
+      const token = localStorage.getItem('google_access_token');
+      if (!token) return;
+      const res = await axios.get(
+        `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`
+      );
+      setPerson(res.data);
     };
-    x();
-  });
+    getData();
+  }, []);
 
-  const onSuccess = (res) => {
-    console.log(res);
-    setProfile(res.profileObj);
+  const handlerLogout = async () => {
+    await googleLogout();
+    console.log('logged out');
   };
 
-  const onFailure = (err) => {
-    console.log('failed', err);
-  };
+  if (Object.values(person).length <= 0)
+    return (
+      <>
+        <GoogleLogin
+          onSuccess={async (credentialResponse) => {
+            console.log(credentialResponse);
+            const res = await axios.get(
+              `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${credentialResponse.credential}`
+            );
+            console.log(res.data);
+            setPerson(res.data);
+            localStorage.setItem(
+              'google_access_token',
+              credentialResponse.credential
+            );
+          }}
+          onError={async () => {
+            console.log('Login Failed');
+          }}
+        />
 
-  const logOut = () => {
-    setProfile(null);
-  };
+        <button onClick={handlerLogout}>Logout</button>
+      </>
+    );
 
   return (
-    <div>
-      <h2>React Google Login</h2>
-      <br />
-      <br />
-      {profile ? (
-        <div>
-          <Image src={profile.imageUrl} alt='user image' />
-          <h3>User Logged in</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email Address: {profile.email}</p>
-          <br />
-          <br />
-          <GoogleLogout
-            clientId={clientId}
-            buttonText='Log out'
-            onLogoutSuccess={logOut}
+    <>
+      <h1>User Info</h1>
+      <ul>
+        <li>
+          <Image
+            src={person.picture}
+            width={300}
+            height={300}
+            alt={'profile'}
           />
-        </div>
-      ) : (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText='Sign in with Google'
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={'single_host_origin'}
-          isSignedIn={true}
-        />
-      )}
-    </div>
+        </li>
+        <li>Username : {person.name}</li>
+        <li>email : {person.email}</li>
+      </ul>
+    </>
   );
 };
 
