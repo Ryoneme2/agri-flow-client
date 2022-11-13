db1051ef4d449785327ad0776c998ec9a1d8ef78
FourthTs <fourth4884@gmail.com>
2022-11-13 19:34:37 +0700

    secion component

--- components/community/Community_block.jsx
+++ components/community/Community_block.jsx
@@ -6,7 +6,7 @@ const CommunityBlock = () => {
     return (
         <>
             <div className='border w-[13.125rem] h-[14.688rem] rounded-t-[0.438rem]'>
-                <div className='rounded-t-[0.438rem] h-full bg-center bg-no-repeat bg-cover relative bg-slate-600'>
+                <div className='rounded-t-[0.438rem] w-full h-full bg-center bg-no-repeat bg-cover relative bg-gradient-to-t from-black to-[#4E4E4E]'>
 
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU7Ypktg4VrGlXLe3IQDHCHM5KdrCyhGbONA&usqp=CAU"
                         alt=""
@@ -14,15 +14,15 @@ const CommunityBlock = () => {
 
                     <div className=' text-white'>
                         <div className='w-full  flex justify-end pr-4'>
-                            <span className='text-[4rem]'>    01   </span>
+                            <span className='text-[4rem] font-light'>    01   </span>
                         </div>
                         <div className='truncate px-2 pt-[3rem]'>
                             <p className='text-[1rem]'>   คนรักโจรปล้นกระถาง   </p>
-                            <p className='text-[0.8rem] truncate'>  aushisnv3rgvuhbviuaehnlbbvhsenblveshnlgasdasdasdasdsfadfafdasdfadsf </p>
+                            <p className='text-[0.8rem] truncate font-light'>  ในประเทศไทยให้พวกเขาได้มีโอกาสศึกษาต่อจนจบการ </p>
                         </div>
                         <div className='flex justify-end  items-baseline p-2'>
                             <div className='text-[0.8rem]'>
-                                สมาชิก
+                                สมาชิกทั้งหมด
                             </div>
                             <div>
                                 <span className='text-[1.25rem] px-2'>   250   </span>
