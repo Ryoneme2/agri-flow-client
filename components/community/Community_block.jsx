113adba87da450f16ac89b7f4fef9a5f33d38ed9
FourthTs <fourth4884@gmail.com>
2022-11-13 19:53:03 +0700

    edit community block

--- components/community/Community_block.jsx
+++ components/community/Community_block.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
-const CommunityBlock = () => {
+const CommunityBlock = ({Communityimg,CommunityMember,CommunityName,CommunityDetail,Rang}) => {
 
 
     return (
         <>
-            <div className='border w-[13.125rem] h-[14.688rem] rounded-t-[0.438rem]'>
+            <div className='border w-[12.55rem] h-[14.688rem] rounded-t-[0.438rem]'>
                 <div className='rounded-t-[0.438rem] w-full h-full bg-center bg-no-repeat bg-cover relative bg-gradient-to-t from-black to-[#4E4E4E]'>
 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU7Ypktg4VrGlXLe3IQDHCHM5KdrCyhGbONA&usqp=CAU"
-                        alt=""
+                    <img src={Communityimg}
+                        alt="Communityimg"
                         className='w-full h-full rounded-t-[0.438rem] absolute mix-blend-overlay' />
 
                     <div className=' text-white'>
                         <div className='w-full  flex justify-end pr-4'>
-                            <span className='text-[4rem] font-light'>    01   </span>
+                            <span className='text-[4rem] font-light'>   0{Rang}   </span>
                         </div>
                         <div className='truncate px-2 pt-[3rem]'>
-                            <p className='text-[1rem]'>   คนรักโจรปล้นกระถาง   </p>
-                            <p className='text-[0.8rem] truncate font-light'>  ในประเทศไทยให้พวกเขาได้มีโอกาสศึกษาต่อจนจบการ </p>
+                            <p className='text-[1rem]'>   {CommunityName}  </p>
+                            <p className='text-[0.8rem] truncate font-light'>  {CommunityDetail} </p>
                         </div>
-                        <div className='flex justify-end  items-baseline p-2'>
+                        <div className='flex justify-end  items-baseline p-2 '>
                             <div className='text-[0.8rem]'>
                                 สมาชิกทั้งหมด
                             </div>
                             <div>
-                                <span className='text-[1.25rem] px-2'>   250   </span>
+                                <span className='text-[1.25rem] px-2'>   {CommunityMember}  </span>
                             </div>
                         </div>
                     </div>
