f8f364a5c9701763ba5c76425b07887fc1a69c88
Palmy <nicha.satt@gmail.com>
2022-11-15 23:13:48 +0700

    fix login responsive again

--- components/Layouts/BoxLogin.jsx
+++ components/Layouts/BoxLogin.jsx
@@ -6,7 +6,7 @@ const BoxLogin = ({ children }) => {
     <div className="bg-gradient-to-r from-[#b6dcfe] to-[#1c658c] h-screen flex justify-center items-center">
       <div className="container p-6 grid grid-cols-12 bg-[#F7FBFC] border-transparent rounded-xl justify-center items-center lg:m-[10rem] m-[2rem] max-w-none pt-[5rem] sm:pt-[6rem] md:pt-6">
         <div className="col-span-12 md:col-span-6 lg:block">
-          <div className="bg-[#F7FBFC] rounded-full flex justify-center items-center mx-auto h-[8rem] w-[8rem] mt-[-4rem] sm:h-[10rem] sm:w-[10rem] sm:mt-[-5rem] md:h-full md:w-full md:mt-0">
+          <div className="bg-[#F7FBFC] rounded-full flex justify-center items-center left-[calc(50%-4rem)] mx-auto h-[8rem] w-[8rem] mt-[-8rem] absolute sm:h-[10rem] sm:w-[10rem] sm:mt-[-9rem] sm:left-[calc(50%-5rem)] md:static md:h-full md:w-full md:mt-0">
             <img src="/images/png/1_2.png" alt="big-agri-flow-logo" />
           </div>
         </div>
