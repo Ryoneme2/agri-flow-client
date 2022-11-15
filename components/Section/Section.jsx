dce1bae90a70343d0e00dd61a30b265a358fd103
Palmy <nicha.satt@gmail.com>
2022-11-15 22:54:17 +0700

    landing non-login responsive

--- components/Section/Section.jsx
+++ components/Section/Section.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const Section = ({context}) => {
-    return (
-        <div className='w-full h-[2.5rem] flex justify-start content-center items-center'>
-            <div className='px-3 w-auto text-[1.5rem] absolute bg-white text-[#1C658C]'>
-                {context}
-            </div>
-            <div className='h-[0.2rem] bg-[#1C658C] w-full' />
-        </div>
-    );
-}
+const Section = ({ context }) => {
+  return (
+    <div className="w-full h-[2.5rem] flex justify-start content-center items-center overflow-hidden">
+      <div className="px-3 w-auto text-[1.5rem] absolute bg-white text-[#1C658C]">
+        {context}
+      </div>
+      <div className="h-[0.2rem] bg-[#1C658C] w-full" />
+    </div>
+  );
+};
 
 export default Section;
-
--- pages/landing/index.jsx
+++ pages/landing/index.jsx
@@ -14,25 +14,26 @@ import CommunityBlock from '../../components/community/Community_block';
 import Profiles from '../../components/staticUser/profiles';
 import ShowUserNum from '../../components/staticUser/showUserNum';
 import Tag from '../../components/Tag';
+import Section from '../../components/Section/Section';
 
-const Landing = () => {
+const LandingNonLogin = () => {
   return (
     <>
       <NavbarNonlogin />
-      <div className="grid grid-cols-12 w-screen mt-5 pr-8">
-        <div className="col-span-3">
-          <div className="grid grid-cols-2 mb-6">
+      <div className="grid grid-cols-12 w-screen mt-5 pr-8 pl-4">
+        <div className="col-span-12 md:col-span-3 flex flex-row md:flex-col">
+          <div className="hidden md:grid grid-cols-2 mb-6 ">
             <ShowUserNum amount={'250000'} name={'บล็อก'} />
             <ShowUserNum amount={'250000'} name={'บัญชีผู้ใช้'} />
             <Profiles
               imgProfile={'/images/profile/jammy.jpg'}
               css={'my-5 col-span-2'}
             />
+            <hr className="col-span-2 w-11/12 h-[2px] bg-[#000000] border-0 m-1" />
           </div>
-          <hr className="my-4 w-10/12 h-[1px] bg-[#A1C0D0] border-0" />
-          <div className="m-5">
-            <h1 className="text-xl">หมวดหมู่</h1>
-            <div className="grid grid-flow-col auto-cols-max">
+          <div className="hidden md:contents mx-4 mb-5 w-full">
+            <h1 className="text-xl overflow-hidden">หมวดหมู่</h1>
+            <div className="flex flex-wrap">
               <Tag linkto={''} tagName={'โรคในพืชยืนต้น'} />
               <Tag linkto={''} tagName={'โรคในพืชล้มลุก'} />
               <Tag linkto={''} tagName={'วัชพืช'} />
@@ -42,18 +43,27 @@ const Landing = () => {
           </div>
         </div>
 
-        <div className="col-span-9">
-          <div className="grid grid-cols-4 overflow-y-auto md:overflow-y-hidden">
+        <div className="col-span-12 md:col-span-9">
+          <div className="grid grid-flow-col justify-between auto-cols-max overflow-x-auto mb-5">
             <CommunityBlock />
             <CommunityBlock />
             <CommunityBlock />
             <CommunityBlock />
           </div>
-          <div>
-            <div className="inline-flex items-center w-screen row">
-              <h1 className="w-1/6 text-center text-xl">บทความน่าสนใจ</h1>
-              <hr className="w-full h-[2px] bg-[#1C658C] border-0" />
+          <div className="flex md:hidden place-items-center mx-3">
+            <h1 className="sm:flex md:hidden hidden text-xl overflow-hidden min-w-[5rem]">
+              หมวดหมู่ :
+            </h1>
+            <div className="flex flex-wrap">
+              <Tag linkto={''} tagName={'โรคในพืชยืนต้น'} />
+              <Tag linkto={''} tagName={'โรคในพืชล้มลุก'} />
+              <Tag linkto={''} tagName={'วัชพืช'} />
+              <Tag linkto={''} tagName={'แมลงศัตรูพืช'} />
+              <Tag linkto={''} tagName={'สัตว์'} />
             </div>
+          </div>
+          <div>
+            <Section context={'บทความน่าสนใจ'} />
             <Blog />
             <Blog />
           </div>
@@ -63,4 +73,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default LandingNonLogin;
