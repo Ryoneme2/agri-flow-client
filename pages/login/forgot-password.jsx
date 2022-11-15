8e0dc53aef5ff009778ba3eaaba5ea93aa9e09e0
Ryoneme2 <worknarair6@gmail.com>
2022-11-15 22:06:32 +0700

    reset password

--- pages/login/forgot-password.jsx
+++ pages/login/forgot-password.jsx
@@ -2,34 +2,47 @@
 import { useEffect, useState } from 'react';
 import InputLogin from '../../components/Login/InputLogin';
 import BoxLogin from '../../components/Layouts/BoxLogin';
+import axios, { AxiosError } from 'axios';
 
 const ForgotPassword = () => {
   // Provide a custom `fetch` implementation as an option
-  const [person, setPerson] = useState({});
+  const [email, setEmail] = useState('');
 
-  if (Object.values(person).length <= 0)
-    return (
-      <BoxLogin>
-        <div className="flex-1 flex-col flex justify-center items-center">
-          <h1 className="font-semibold sm:text-[1.7rem] text-[#1C658C] my-3">
-            ลืมรหัสผ่าน
-          </h1>
-          <InputLogin type={'text'} placeholder={'อีเมล...'} />
-          <InputLogin
-            type={'submit'}
-            value={'ยืนยัน'}
-            css={'justify-center text-center'}
-            color={'submit'}
-          />
-        </div>
-      </BoxLogin>
-    );
+  const onClickHandle = async () => {
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/resetPasswordRequest`,
+        {
+          email,
+        }
+      );
+    } catch (e) {
+      if (e instanceof AxiosError) {
+      }
+    }
+  };
 
   return (
-    <>
-      <h1>ok</h1>
-      <button onClick={handlerLogout}>Logout</button>
-    </>
+    <BoxLogin>
+      <div className="flex-1 flex-col flex justify-center items-center">
+        <h1 className="font-semibold sm:text-[1.7rem] text-[#1C658C] my-3">
+          ลืมรหัสผ่าน
+        </h1>
+        <InputLogin
+          onChange={(e) => setEmail(e.target.value)}
+          type={'text'}
+          value={email}
+          placeholder={'อีเมล...'}
+        />
+        <InputLogin
+          onClick={onClickHandle}
+          type={'submit'}
+          value={'ยืนยัน'}
+          css={'justify-center text-center'}
+          color={'submit'}
+        />
+      </div>
+    </BoxLogin>
   );
 };
 
--- pages/login/resetPassword/[token].jsx
+++ pages/login/resetPassword/[token].jsx
@@ -3,10 +3,31 @@ import { useEffect, useState } from 'react';
 import InputLogin from '../../../components/Login/InputLogin';
 import BoxLogin from '../../../components/Layouts/BoxLogin';
 import { useRouter } from 'next/router';
+import axios, { AxiosError } from 'axios';
 
 const ResetPassword = () => {
   const router = useRouter();
   const { token } = router.query;
+
+  const [password, setPassword] = useState('');
+
+  const onClickHandle = async () => {
+    try {
+      console.log(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/resetPass`);
+
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/resetPass`,
+        {
+          token,
+          password,
+        }
+      );
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        console.error(e);
+      }
+    }
+  };
 
   if (!token)
     return (
@@ -24,17 +45,24 @@ const ResetPassword = () => {
           <h1 className="font-semibold sm:text-[1.7rem] text-[#1C658C] my-3">
             เปลี่ยนรหัสผ่าน
           </h1>
-          <InputLogin type={'text'} placeholder={'รหัสผ่านใหม่...'} />
+          <InputLogin
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type={'text'}
+            placeholder={'รหัสผ่านใหม่...'}
+          />
           <InputLogin type={'text'} placeholder={'ยืนยันรหัสผ่าน...'} />
           <InputLogin
-            type={'submit'}
+            onClick={onClickHandle}
+            type={'button'}
             value={'ยืนยัน'}
             css={'justify-center text-center'}
             color={'submit'}
           />
         </div>
       </BoxLogin>
-      ;
     </>
   );
 };
