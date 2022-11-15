fc1d85bd0cf3cbfa74e8f74577c9057923def552
FourthTs <fourth4884@gmail.com>
2022-11-15 23:21:48 +0700

    edit line icon

---
+++ components/Blog/miniBlog.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const MiniBlog = () => {
+    return (
+        <div>
+            <div className='w-full h-full ml-2 flex-col item-center'>
+                    {/* Header */}
+                    <div className='w-full flex flex-col sm:flex-row  justify-between items-center '>
+                        <div className='w-full'>
+                            <p className='text-[1.75rem] m-0 p-0 truncate'>จิ๊กโก๋สัมนาเบบี้ปัจฉิมนิเทศ</p>
+                        </div>
+                        <div className='w-full sm:w-50 flex justify-start sm:justify-end'>
+                            <Tag linkto={''} tagName={'รักน้ำรักปลา'} />
+                        </div>
+
+                    </div>
+                    {/* detail */}
+                    <div className='w-full'>
+                        <div className='h-[7rem] w-[90%] overflow-y-hidden '>
+                            <p className='text-[#979797] text-[1.2rem] font-light m-0 p-0 overflow-y-hidden'>
+                                {text}
+                            </p>
+                        </div>
+                    </div>
+                    {/* footer */}
+                    <div className='w-full'>
+                        <div className='w-full  flex justify-between items-center mt-4'>
+                            <div className='flex items-center'>
+                                <div>
+                                    <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAA
+                                    JbSJIAAAAM1BMVEXk5ueutLeqsbTn6eqpr7PJzc/j5ebf4eLZ3N2wtrnBxsjN0NLGysy6v8HT
+                                    1tissra8wMNxTKO9AAAFDklEQVR4nO2d3XqDIAxAlfivoO//tEOZWzvbVTEpic252W3PF0gAI
+                                    csyRVEURVEURVEURVEURVEURVEURVEURVEURVEURflgAFL/AirAqzXO9R7XNBVcy9TbuMHmxj
+                                    N6lr92cNVVLKEurVfK/zCORVvW8iUBnC02dj+Wpu0z0Y6QlaN5phcwZqjkOkK5HZyPAjkIjSO
+                                    4fIdfcOwFKkJlX4zPu7Ha1tIcwR3wWxyFhRG6g4Je0YpSPDJCV8a2Sv2zd1O1x/2WMDZCwljH
+                                    +clRrHfWCLGK8REMiql//2si5+DKWKcWeAGcFMzzNrXC/0TUwQ2s6+LhlcwjTMlYsUIQzPOCb
+                                    7YBiyHopyLXIEKPEkI/TgeuiidK/R9FniUDOjRDpvm0RhqjMyyXNjDhCfIMYl1gGjIMIuYsnG
+                                    EYRMRZOMMunaLVwpWRW008v6fYKDIzxCwVAeNSO90BJW6emelYBRF/kHpYGVaoxTDAaxOFsfP
+                                    9y8hpJ4xd7gOcij7JNGQ1EYFgkPJa1jQEiYZXRaRINKxSDUW9n+FT82lSKadkiru9/4XPqSLW
+                                    OekGPoY05TAvLm9orm+YWuwHoBHkZKijNBJGmeb61eL6Ff/6q7bLr7yvv3vKGhpDRjvgjGaPz
+                                    +gUg6YgcvpyAR2FIZ9U6nEEyZRTovmEU32KichpGn7C17XrfyH9gK/c0CMP05HZIM2uf9sEve
+                                    izKveBy9/6Qt7o89ne33D525cfcIMW6ab+TMEukQbQbu+xu7X3A9bChmWaCeAkG17bpntwXgW
+                                    xHaMzGPmUaR5dQZiKqRVeUZ3047fi3nAu28h4CHxCsZAgmEH8Y27jJAhm8c+5RQzRQNVGhVFS
+                                    fxOYIjp/pP7RxzjevYXVGf4eLt+BJ1vCuLuLkrgABgCGXZ2wik5uty+oBvNirI6mkzhAf4Gsb
+                                    58Hcm67Jzd+KwD10BYPLL3e0MjvKrgAULnOfveF/O4N2Xb9BZom3gJes3F9X5Zze8/6Yt09b4
+                                    CrqsEjUv8oFBaR2rl+6CZr2xVrp24o/WitBKuGrrpl1+bFkmK2qXTON4VpbdfLa7o7y/WdLxG
+                                    7lm2Lqh2clOwTegbvc/vj2U78CwhA87Bn8G5Nk3eOb0Nsr9flz3sG78UUtue4kpv1xvjg3TMa
+                                    y62BMlTlP+vrOMnJsRmt/ze0jsfkPPYdAH57hK+34PeOyc8XIXu5xT2HsUkdZz+adwg8HGFfQ
+                                    3K5jtDvbUiO4Di9/ywHGrL88pDizZ++oTp+an+SMX/ndymUCwmHMdO7yuOx83pUx/eEMU0Avx
+                                    WndwgidAqOZ8ypCwdEfvvEo6D9HwpA8wzvmOJEqAg9ySu8g4x0Hb9hSB/BANEKJ+LbPBU0lzb
+                                    AJs4xt1AoshKkUGQmiH8/jJ0gdhTTLmSegHlPE0oOdXALnqDjKYh3px//fSgSWG8UqfrrIICz
+                                    YYSJXRr9BSPbpNzw7gBjKjKOYI7ReIGqQRIap5+5MdjyvuDkExvGeXSlONWZAP3/AZBwJohU7
+                                    QJRGU+cTVH18ELmRPNBmibW6MT/k1b0XhdkRBvyT6SB6EYv/GvhSmRNpGngRULsAlxMCGNXp7
+                                    w3FfdEbTEEDdLI9TdIKRUzUesa3I461ER8cpNT7gMRhpKmYVS9ELOgCUQsa4SsulciKiLbY+A
+                                    nHD8cpuhISsnxpamI84sbDq9qYJgf8wiiOBrC7Ml7M7ZECCqKoiiKoiiKoiiKoijv5AvJxlZR
+                                    yNWWLwAAAABJRU5ErkJggg=="
+                                        width={31} height={31}
+                                        className='rounded-full' />
+                                </div>
+                                <div className='px-3 text-[#7A7A7A]'>
+                                    {`ชื่อผู้เขียน`}
+                                </div>
+                                <div className='px-3 text-[#7A7A7A]'>
+                                    {`เวลาที่เผยแพร่`}
+                                </div>
+                            </div>
+                            <div className='mr-2 sm:mr-0'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-plus"
+                                    width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1C658C" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                                    <line x1="12" y1="11" x2="12" y2="17" />
+                                    <line x1="9" y1="14" x2="15" y2="14" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+}
+
+export default MiniBlog;
--- components/Navbar/NavbarNonlogin.jsx
+++ components/Navbar/NavbarNonlogin.jsx
@@ -10,21 +10,21 @@ const NavbarNonlogin= () => {
        <> 
             <div className="border-b w-full h-[80px] flex justify-between">
 
-                <div>
+                <div className='w-full h-full pt-[7px]'>
                     <Link href="/Home">
                         <Image src={'/images/png/2-2E.png'} width={160} height={100} />
                     </Link>
                 </div>
 
-                <div className=' h-[50%] flex justify-around my-auto'>
-                    <div className=' flex my-auto px-5'>
-                        <Link href="/login">
-                            <span className='underline font-bold'>ลงชื่อเข้าใช้งาน</span>
+                <div className='flex w-full justify-end '>
+                    <div className='my-auto w-auto hidden sm:block text-right mx-2 truncate'>
+                        <Link href="/login/signup">
+                            <span className='underline font-bold truncate text-[#1C658C]'>สมัครเข้าใช้งาน</span>
                         </Link>
                     </div>
-                    <div className='px-5'>
-                        <Link href="/login/signup">
-                            <Button className="text-[1rem] font-bold" context={'สมัครเข้าใช้งาน'} onClick={''} />
+                    <div className='my-auto w-auto flex justify-end mx-2 truncate'>
+                        <Link href="/login">
+                            <Button className="text-[1rem] font-bold " context={'ลงชื่อเข้าใช้งาน'} />
                         </Link>
                     </div>
                 </div>
