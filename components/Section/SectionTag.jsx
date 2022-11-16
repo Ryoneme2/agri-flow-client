7f9a66781cc723d0fdece1f889c7087616347a2e
FourthTs <fourth4884@gmail.com>
2022-11-16 18:15:49 +0700

    edit tag style

---
+++ components/Section/SectionTag.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionTag = ({ text , tag}) => {
+  return (
+    <div className="w-full h-[2.5rem] flex justify-start content-center items-center overflow-hidden">
+      <div className="px-3 w-auto text-[1.5rem] absolute bg-white text-[#1C658C]">
+        {text} <span> - </span> {tag}
+      </div>
+      <div className="h-[0.2rem] bg-[#1C658C] w-full" />
+    </div>
+  );
+};
+
+export default SectionTag;
