218e75cb6b1f1dfaf6e3988418abd30e483c952a
Palmy <nicha.satt@gmail.com>
2022-11-16 22:17:00 +0700

    rename file name Home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Homr = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
-}
+import Home_login from '../components/Home/Home_login';
+import Home_nonlogin from '../components/Home/Home_nonlogin';
+import SuggustTopic from '../components/Home/suggustTopic';
+import Blog from '../components/Blog/Blog';
 
-export default Homr;
+const LandingNonLogin = () => {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const localToken = localStorage.getItem('access_token');
+    setToken(localToken);
+  }, []);
+
+  if (token) return <Home_login />;
+  return (
+    <Home_nonlogin
+      blog={
+        <SuggustTopic
+          Topic={'บทความตาม หมวดหมู่'}
+          Children={
+            <>
+              <Blog />
+            </>
+          }
+        />
+      }
+    />
+  );
+};
+
+export default LandingNonLogin;
