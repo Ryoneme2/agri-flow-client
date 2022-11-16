bbf5890989fa6da53147802600c5416b87f75fcc
Palmy <nicha.satt@gmail.com>
2022-11-16 19:11:48 +0700

    landing page ui done

--- components/Home/Home_login.jsx
+++ components/Home/Home_login.jsx
@@ -9,23 +9,26 @@ const NavbarNonlogin = dynamic(() => import('../Navbar/NavbarNonlogin'), {
 });
 import Blog from '../Blog/Blog';
 import CommunityBlock from '../community/Community_block';
-import Profiles from '../staticUser/Profiles';
-import ShowUserNum from '../staticUser/ShowUserNum';
 import Tag from '../Tag';
 import Section from '../Section/Section';
 import SuggustTopic from './suggustTopic';
 import AvatarText from '../staticUser/AvatarText';
+import UpgateAccount from '../Sidebar/UpgateAccount';
 
-const LandingNonLogin = () => {
+const LandingNonLogin = ({ blog }) => {
+  let content = '';
+  blog ? (content = 'hidden') : (content = '');
+
   return (
     <>
       <Navbarlogin />
       <div className="grid grid-cols-12 w-full sm:w-[90%] mx-auto mt-5 pr-8 pl-4">
-        <div className="col-span-12 md:col-span-3 flex flex-row md:flex-col">
-          <div className="hidden md:grid mb-6 ">
-            <div className="hidden md:grid w-full">
+        <div className="col-span-12 md:col-span-3 hidden md:flex flex-row md:flex-col">
+          <div className="grid mb-6">
+            <UpgateAccount />
+            <div className="w-full">
               <h1 className="text-xl overflow-hidden">บุคคลที่ติดตาม</h1>
-              <div className="">
+              <div>
                 <AvatarText
                   imgProfile={'/images/profile/jammy.jpg'}
                   name={'Pummy'}
@@ -41,7 +44,7 @@ const LandingNonLogin = () => {
               </div>
             </div>
           </div>
-          <div className="hidden md:contents mx-4 mb-5 w-full">
+          <div className="contents mx-4 mb-5 w-full">
             <h1 className="text-xl overflow-hidden">หมวดหมู่ที่เข้าชมบ่อย</h1>
             <div className="flex flex-wrap">
               <Tag linkto={''} tagName={'โรคในพืชยืนต้น'} />
@@ -54,7 +57,9 @@ const LandingNonLogin = () => {
         </div>
 
         <div className="col-span-12 md:col-span-9">
-          <div className="grid grid-flow-col justify-between auto-cols-max overflow-x-auto mb-5">
+          <div
+            className={`${content} grid grid-flow-col justify-between auto-cols-max overflow-x-auto mb-5`}
+          >
             <CommunityBlock />
             <CommunityBlock />
             <CommunityBlock />
@@ -72,25 +77,27 @@ const LandingNonLogin = () => {
               <Tag linkto={''} tagName={'สัตว์'} />
             </div>
           </div>
-
-          <SuggustTopic
-            Topic={'บทความสำหรับคุณ'}
-            Children={
-              <>
-                <Blog />
-                <Blog />
-              </>
-            }
-          />
-          <SuggustTopic
-            Topic={'บทความที่คุณติดตาม'}
-            Children={
-              <>
-                <Blog />
-              </>
-            }
-          />
-          <SuggustTopic Topic={'บทความน่าสนใจ'} />
+          <div className={content}>
+            <SuggustTopic
+              Topic={'บทความสำหรับคุณ'}
+              Children={
+                <>
+                  <Blog />
+                  <Blog />
+                </>
+              }
+            />
+            <SuggustTopic
+              Topic={'บทความที่คุณติดตาม'}
+              Children={
+                <>
+                  <Blog />
+                </>
+              }
+            />
+            <SuggustTopic Topic={'บทความน่าสนใจ'} />
+          </div>
+          {blog}
         </div>
       </div>
     </>
--- pages/landing/index.jsx
+++ pages/landing/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import Home_login from '../../components/Home/Home_login';
 import Home_nonlogin from '../../components/Home/Home_nonlogin';
+import SuggustTopic from '../../components/Home/suggustTopic';
+import Blog from '../../components/Blog/Blog';
 
 const LandingNonLogin = () => {
   const [token, setToken] = useState('');
@@ -12,7 +14,20 @@ const LandingNonLogin = () => {
   }, []);
 
   if (token) return <Home_login />;
-  return <Home_nonlogin />;
+  return (
+    <Home_nonlogin
+      blog={
+        <SuggustTopic
+          Topic={'บทความตาม หมวดหมู่'}
+          Children={
+            <>
+              <Blog />
+            </>
+          }
+        />
+      }
+    />
+  );
 };
 
 export default LandingNonLogin;
