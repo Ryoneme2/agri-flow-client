89a133aa30c9d1b18e878a33382b3b3a0b4fec30
Palmy <nicha.satt@gmail.com>
2022-11-17 16:46:01 +0700

    เจมแก้แล้ว

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import dynamic from 'next/dynamic';
 
 const HomeComponent = dynamic(() => import('../components/Home'), {
   ssr: false,
 });
 
 export default function Home() {
-  return (
-    <Home_nonlogin
-      blog={
-        <SuggustTopic
-          Topic={'บทความตาม หมวดหมู่'}
-          Children={
-            <>
-              <Blog />
-            </>
-          }
-        />
-      }
-    />
-  );
+  return <HomeComponent />;
 }
