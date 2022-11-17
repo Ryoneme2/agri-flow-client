d31879d07045cf8608834c18aaf5266926764b29
Ryoneme2 <worknarair6@gmail.com>
2022-11-17 19:35:00 +0700

    update tmp image

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -16,7 +16,7 @@ const Blog = ({ blog }) => {
     );
 
   const rawText = blog?.blogContent?.content || 'หาไม่เจอ';
-  const firstImage = blog?.thumbnail[0] || '';
+  const firstImage = blog?.thumbnail[0] || '/images/png/tmp-image.png';
   const otherImage =
     blog?.thumbnail?.slice(
       1,
@@ -43,20 +43,18 @@ const Blog = ({ blog }) => {
             />
           </div>
           <div className="w-full flex justify-start content-center items-center">
-            {
-              otherImage.map((img, i) => {
-                return (
-                  <Image
-                    key={img}
-                    src={img}
-                    width={40}
-                    height={40}
-                    className="rounded-[7px] mt-2 mr-2 border border-gray-400 w-[40px] h-[40px]"
-                    alt={`sub thumbnail-${i}`}
-                  />
-                );
-              })
-            }
+            {otherImage.map((img, i) => {
+              return (
+                <Image
+                  key={img}
+                  src={img}
+                  width={40}
+                  height={40}
+                  className="rounded-[7px] mt-2 mr-2 border border-gray-400 w-[40px] h-[40px]"
+                  alt={`sub thumbnail-${i}`}
+                />
+              );
+            })}
           </div>
         </div>
 
@@ -64,7 +62,7 @@ const Blog = ({ blog }) => {
           {/* Header */}
           <div className="w-full flex flex-col sm:flex-row  justify-between items-center ">
             <div className="w-full">
-              <p className="text-[1.75rem] m-0 p-0 truncate">
+              <p className="text-[1.75rem] ml-3 mb-0 mt-0 p-0 truncate">
                 {blog.blogContent.title}
               </p>
             </div>
@@ -76,7 +74,7 @@ const Blog = ({ blog }) => {
             </div>
           </div>
           {/* detail */}
-          <div className="w-full">
+          <div className="w-full ml-3">
             <div className="h-[7rem] w-[90%] overflow-y-hidden ">
               <p className="text-[#979797] text-[1.2rem] font-light m-0 p-0 overflow-y-hidden">
                 {text}
