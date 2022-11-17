bb12069e81fbf3ddddf84bcc6a69ea46691b0773
Palmy <nicha.satt@gmail.com>
2022-11-17 20:47:45 +0700

    commu init

---
+++ components/community/Commu_sidebar.jsx
@@ -0,0 +1,19 @@
+import { Image } from '@mantine/core';
+
+const CommunitySidebar = () => {
+  return (
+    <>
+      <div>
+        <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+          <Image
+            radius="md"
+            src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+            alt="Page commu"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommunitySidebar;
--- pages/Community.jsx
+++ pages/Community.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import CommuSidebar from '../components/community/Commu_sidebar';
 
 const Commu = () => {
-    return (
-        <div>
-            Community
-        </div>
-    );
-}
+  return (
+    <>
+      <div>Community</div>
+      <CommuSidebar />
+    </>
+  );
+};
 
 export default Commu;
