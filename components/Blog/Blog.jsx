842bee946eb0d567acd58f26df9986d56bcdc937
FourthTs <fourth4884@gmail.com>
2022-11-18 14:28:50 +0700

    svg edit

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -102,15 +102,15 @@ const Blog = ({ blog }) => {
               <div className="mr-2 sm:mr-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-file-plus"
+                  className="icon icon-tabler icon-tabler-file-plus"
                   width="35"
                   height="35"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="#1C658C"
                   fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M14 3v4a1 1 0 0 0 1 1h4" />
