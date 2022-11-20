262ad86c4cb809a5c5e88afa16b45c9adfe9c50c
FourthTs <fourth4884@gmail.com>
2022-11-20 12:49:19 +0700

    edit profile

--- pages/EditProfile.jsx
+++ pages/EditProfile.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
+const EditProfilecomponnection = dynamic(
+  () => import('../components/EditProfile/Editprofile'),
+  { ssr: false, }
+);
+const Section = dynamic(() => import('../components/Section/Section'), { ssr: false });
+
 
 const EditProfile = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='w-[90%] mx-auto my-2 py-1 border-b-2 border-[#1C658C] text-[#1C658C]'>
+                แก้ไขโปรไฟล์ผู้ใช้
+            </div>
+            <EditProfilecomponnection/>
+        </>
     );
 }
 
